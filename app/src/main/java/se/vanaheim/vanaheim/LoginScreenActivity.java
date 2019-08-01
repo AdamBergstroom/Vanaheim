2f47b84afa825505eeb5b344ba19a60976c732a5
TrevorD832 <43454227+TrevorD832@users.noreply.github.com>
2019-08-01 21:44:36 +0200

    new login and firebase

--- app/src/main/java/se/vanaheim/vanaheim/LoginScreenActivity.java
+++ app/src/main/java/se/vanaheim/vanaheim/LoginScreenActivity.java
@@ -2,43 +2,101 @@ package se.vanaheim.vanaheim;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginScreenActivity extends AppCompatActivity {
 
-    private String passwordValue;
-    private EditText passwordET;
-    private Button loginButton;
+    private EditText email, password;
+    private Button btnSignIn;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        setContentView(R.layout.login_screen_testing);
 
-        passwordET = findViewById(R.id.password);
-        loginButton = findViewById(R.id.login_button);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.emailId);
+        password = findViewById(R.id.passwordId);
+        btnSignIn = findViewById(R.id.btnSignIn);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                // Check if the user exists inside the Firebase Authentication
+                if(mFirebaseUser != null){
+                    Toast.makeText(LoginScreenActivity.this,
+                            "You are logged in",Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(LoginScreenActivity.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginScreenActivity.this,
+                            "Access Denied",Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                String emailValue = email.getText().toString();
+                String passwordValue = password.getText().toString();
 
-                passwordValue = passwordET.getText().toString();
-
-                if(passwordValue.equals("gavle#VH2006")) {
-                    Intent intent = new Intent(LoginScreenActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                else{
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "Fel lösenord",
-                            Toast.LENGTH_SHORT);
-                    toast.show();
+                if(emailValue.isEmpty()){
+                    Toast.makeText(LoginScreenActivity.this,
+                            "Please enter email",Toast.LENGTH_LONG).show();
+                } else if(passwordValue.isEmpty()){
+                    Toast.makeText(LoginScreenActivity.this,
+                            "Please enter password",Toast.LENGTH_LONG).show();
+                }else if(emailValue.isEmpty() && passwordValue.isEmpty()){
+                    Toast.makeText(LoginScreenActivity.this,
+                            "Fields Empty",Toast.LENGTH_LONG).show();
+                }else if(!(emailValue.isEmpty() && passwordValue.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(emailValue,passwordValue)
+                            .addOnCompleteListener(LoginScreenActivity.this,
+                                    new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginScreenActivity.this,
+                                        "Login Error, Please Login Again",Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(LoginScreenActivity.this,
+                                        "Login Worked!",Toast.LENGTH_LONG).show();
+                                Intent i = new Intent(LoginScreenActivity.this, MainActivity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(LoginScreenActivity.this,
+                            "Error Occurred!",Toast.LENGTH_LONG).show();
                 }
             }
         });
+
     }
 }
